57c4a0bc86884f2a641931004cf61dd89f1df89c
Yukkiu <super.11332255@gmail.com>
2023-09-03 13:25:53 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+   println("hai")
+    println("kak dela")
+    println("kak pogoda")
+    // println("azaza"
+    print("a tut vse")
+    print("budet")
+    print("v 1")
+    print("stroku")
+    /*print("ya genius")
+     print("o da")*/
+    println("huehuehue")
+}
